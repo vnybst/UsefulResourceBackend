24d9687ff25d4d11ca102bc7e61ea2f03893e59e
Sourabh Semalty <semaltysourabh@gmail.com>
2023-12-24 21:37:14 +0530

    feat: removed unneeed subcategory model

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -1,17 +1,14 @@
 import SequelizeConnection from "../connection";
 import CategoryDetails from "./CategoryDetails";
-import SubCategoryDetails from "./SubCategoryDetails";
 import UserDetails from "./UserDetails";
 
 const sequelize = SequelizeConnection.getInstance();
 
 UserDetails.initModel(sequelize);
 CategoryDetails.initModel(sequelize);
-SubCategoryDetails.initModel(sequelize);
 
 export const db = {
   sequelize,
   UserDetails,
   CategoryDetails,
-  SubCategoryDetails,
 };
