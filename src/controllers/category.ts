262fd58e47325e075ce8f5e3f094da4bb8ce9d40
Sourabh Semalty <semaltysourabh@gmail.com>
2023-12-24 22:34:12 +0530

    feat: added category route method and api

--- src/controllers/category.ts
+++ src/controllers/category.ts
@@ -2,13 +2,34 @@ import { Request, Response } from "express";
 import { db } from "../database/models";
 
 export async function getAllCategory(req: Request, res: Response) {
-  const categoriesData = await db.CategoryDetails.findAll();
+  const categoriesData = await db.CategoryDetails.findAll({ raw: true });
   if (!categoriesData.length)
     return res.status(404).json({ message: "no categories available!" });
 
-  return res.status(200).json(categoriesData);
+  const organizeCategories = (categories, parentId = null) => {
+    const result = [];
+    categories.forEach((category) => {
+      if (category.parentCategoryId === parentId) {
+        const subcategories = organizeCategories(categories, category.id);
+        console.log(subcategories);
+        if (subcategories.length > 0) {
+          category.subcategories = subcategories;
+        }
+        result.push(category);
+      }
+    });
+    return result;
+  };
+  const nestedCategories = organizeCategories(categoriesData);
+  return res.status(200).json(nestedCategories);
 }
 
-export function getCategoryById() {
-  return { status: "success" };
+export async function getCategoryById(req: Request, res: Response) {
+  const { id } = req.params;
+  const category = await db.CategoryDetails.findByPk(id);
+
+  if (!category)
+    return res.status(404).json({ message: "category doesn't not exists !" });
+
+  return res.status(200).json(category);
 }
